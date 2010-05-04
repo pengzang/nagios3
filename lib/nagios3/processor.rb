2cc68c17491b16934847b90a30fbf425b9a3fc60
Brian J Reath <brian.reath@ccisystems.com>
2010-05-03 22:22:23 -0500

    Fleshing out initial pass at the perfdata processor

--- lib/nagios3/processor.rb
+++ lib/nagios3/processor.rb
@@ -1,3 +1,8 @@
+require 'net/http'
+require 'logger'
+require 'logrotate'
+require 'zlib'
+
 module Nagios3
   #[SERVICEPERFDATA]\t$TIMET$\t$HOSTNAME$\t$SERVICEDESC$\t$_SERVICE_ID$\t$_SERVICERRD$\t$_SERVICERRDFILE$\t$_SERVICESTATS$\t$SERVICEEXECUTIONTIME$\t$SERVICELATENCY$\t$SERVICEOUTPUT$\t$SERVICEPERFDATA$
   class ServicePerfdataProcessor
@@ -9,8 +14,9 @@ module Nagios3
   
     def run
       parse_file
-      process_dbs
       clear_file
+      
+      
       
       # Do a logrotate on the file
       # Gather data and compress
@@ -26,24 +32,7 @@ module Nagios3
 
     def parse_file
       File.open(@file_path) do |f|
-        f.each { |line| @entries << parse_line(line) }
-      end
-    end
-
-    def process_dbs
-      @entries.each do |entry|
-        # Only update RRDs for service perfdata entries that are enabled for RRD
-        p entry
-        unless entry.nil? 
-          if entry[:rrd] == "1"
-            begin
-              perf_value = entry[:perfdata].split("=")[1].to_i.to_s
-              RRD.update("#{entry[:rrd_file]}", "#{entry[:time]}:#{perf_value}") if File.exists?(entry[:rrd_file])
-            rescue => ex
-              # Simply ignore the entry if it raises an exception
-            end
-          end
-        end
+        f.each { |line| @entries << parse(line) }
       end
     end
   
@@ -51,10 +40,9 @@ module Nagios3
       File.open(@file_path, "w") do |f|
         f.print ""
       end
-    
     end
 
-    def parse_line(line)
+    def parse(line)
       if line =~ /^\[SERVICEPERFDATA\]([^\|]*)\|([^\|]*)\|([^\|]*)\|([^\|]*)\|([^\|]*)\|([^\|]*)\|([^\|]*)\|([^\|]*)\|([^\|]*)\|([^\|]*)\|([^\|]*)$/
         perf_hash = { :time => $1, :host => $2, :service => $3, :id => $4, :rrd => $5, :rrd_file => $6,
                       :stats => $7, :execution_time => $8, :latency => $9, :output => $10, :perfdata => $11 }
