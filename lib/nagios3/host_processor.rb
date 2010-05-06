8c39d7c65f69d4e69fd3668cba5d9de57a092d4f
Brian J Reath <brian.reath@ccisystems.com>
2010-05-06 00:38:01 -0500

    Don't send HUP to the nagios process

--- lib/nagios3/host_processor.rb
+++ lib/nagios3/host_processor.rb
@@ -14,15 +14,7 @@ module Nagios3
     
   private
     def rotate_file
-      block = Proc.new() do
-        File.open(Nagios3.pid_file) do |pid_stream|
-          pid = pid_stream.read().to_i()
-          Process.kill("HUP", pid)
-        end
-      end
-      
-      options = { :post_rotate => block }
-      LogRotate.rotate_file(Nagios3.host_perfdata_path, options)
+      LogRotate.rotate_file(Nagios3.host_perfdata_path)
     end
     
     def parse_files
